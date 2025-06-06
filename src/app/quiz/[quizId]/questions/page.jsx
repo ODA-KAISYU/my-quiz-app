d6638024614d236372c3bf6f28b812a139f1b296
OdaKaisyu <kai20041008@icloud.com>
2025-06-06 19:36:34 +0900

    🎨 スタイルを整えた

--- src/app/quiz/[quizId]/questions/page.jsx
+++ src/app/quiz/[quizId]/questions/page.jsx
@@ -3,26 +3,41 @@
 import { useParams } from "next/navigation";
 import React from "react";
 import quizzes from "@/app/data/quizzes";
+import { useRouter } from "next/navigation";
+import styles from "./QuizQuestionsPage.module.css"
 
 export default function QuizQuestionsPage() {
     const { quizId } = useParams();
+    const router = useRouter();  // ここでRouter習得
+
     const quiz = quizzes[quizId]; // quizに応じて選択
     
     return (
-        <main>
-            <h1>{quiz.title}</h1>
+        <main className={styles.main}>
+            <h1 className={styles.title}>{quiz.title}</h1>
             <section>
-                <p>{quiz.description}</p>
-                <ol>
+                <p className={styles.description}>{quiz.description}</p>
+                <ol className={styles.answerList}>
                     {quiz.answers.map((q) => (
-                        <li key={q.number}>
+                        <li key={q.number} className={styles.answerItem}>
+                            <div className={styles.answerNumber}>
+                                {q.number}.
+                            </div>
                           <input type="text"
                                  id={`answer-${q.number}`}
-                                 placeholder={`(${q.number})`} />  
+                                 className={styles.answerInput}
+                                 />  
                         </li>
                     ))}
                 </ol>
             </section>
+            <button
+            className={styles.button}
+            onClick={() => {
+                router.push(`/quiz/${quizId}/result`); //ここでページ遷移
+            }}
+            >解答終了
+            </button>
         </main>
     );
 }
---
+++ src/app/quiz/[quizId]/result/page.jsx
@@ -0,0 +1,12 @@
+'use client';
+
+import React from "react";
+
+export default function QuizResultPage() {
+    return (
+        <main>
+            <h1>結果ページへ</h1>
+            <p>ここに結果を表示します。</p>
+        </main>
+    );
+}
